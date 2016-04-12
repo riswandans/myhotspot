03434ab7a37e6197c28f5dff779247dac42cf177
Riswanda N.S <root.devilscream@gmail.com>
2016-04-12 15:25:19 +0800

    *
    first commit

---
+++ MyHotspot/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.ssid_name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.cmdExit = new System.Windows.Forms.Button();
+            this.cmdExe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(122, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hotspot Name";
+            // 
+            // ssid_name
+            // 
+            this.ssid_name.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ssid_name.Location = new System.Drawing.Point(67, 204);
+            this.ssid_name.Name = "ssid_name";
+            this.ssid_name.Size = new System.Drawing.Size(222, 20);
+            this.ssid_name.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(136, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // password
+            // 
+            this.password.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.password.Location = new System.Drawing.Point(67, 276);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(222, 20);
+            this.password.TabIndex = 3;
+            // 
+            // cmdExit
+            // 
+            this.cmdExit.Location = new System.Drawing.Point(200, 332);
+            this.cmdExit.Name = "cmdExit";
+            this.cmdExit.Size = new System.Drawing.Size(89, 28);
+            this.cmdExit.TabIndex = 4;
+            this.cmdExit.Text = "Exit";
+            this.cmdExit.UseVisualStyleBackColor = true;
+            // 
+            // cmdExe
+            // 
+            this.cmdExe.Location = new System.Drawing.Point(67, 332);
+            this.cmdExe.Name = "cmdExe";
+            this.cmdExe.Size = new System.Drawing.Size(85, 28);
+            this.cmdExe.TabIndex = 5;
+            this.cmdExe.Text = "Start";
+            this.cmdExe.UseVisualStyleBackColor = true;
+            this.cmdExe.Click += new System.EventHandler(this.cmdExe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(357, 404);
+            this.Controls.Add(this.cmdExe);
+            this.Controls.Add(this.cmdExit);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ssid_name);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "MyHotspot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ssid_name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button cmdExit;
+        private System.Windows.Forms.Button cmdExe;
+    }
+}
+
---
+++ MyHotspot/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void execute_cmd(string command)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C " + command;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdExe_Click(object sender, EventArgs e)
+        {
+            if (cmdExe.Text == "Start")
+            {
+                cmdExe.Text = "Stop";
+                execute_cmd("netsh wlan set hostednetwork mode=allow ssid=" + ssid_name.Text + " key=" + password.Text);
+                execute_cmd("netsh wlan start hostednetwork");
+            } else
+            {
+                cmdExe.Text = "Start";
+                execute_cmd("netsh wlan stop hostednetwork");
+            }
+        }
+    }
+}
